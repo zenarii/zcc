87ef266b99e4a60304534593bccd44f79f242726
abiab <abiabaxter@gmail.com>
2022-11-13 17:17:09 +0000

    Recreate test_unary script after accidental deletion

--- test_programs/unary_operator_tests/test_unary.sh
+++ test_programs/unary_operator_tests/test_unary.sh
@@ -4,28 +4,27 @@ CLEAR="\033[0m"
 
 # test_program source_file.c expected_value
 function test_program () {
-	~/dev/ccompiler/build/zcc --delete-asm $1
-	./out > /dev/null
-	returned_value=$?
-	if [ $returned_value == $2 ]
-	then
-		echo -e "${GREEN}[PASSED] $1 ${CLEAR}"
-	else
-		echo -e "${RED}[FAILED] $1 expected $2, got $returned_value ${CLEAR}"
-	fi
-	rm out
+       ~/dev/ccompiler/build/zcc --delete-asm $1
+       ./out > /dev/null
+       returned_value=$?
+       if [ $returned_value == $2 ]
+       then
+               echo -e "${GREEN}[PASSED] $1 ${CLEAR}"
+       else
+               echo -e "${RED}[FAILED] $1 expected $2, got $returned_value ${CLEAR}"
+       fi
+       rm out
 }
 
 function test_invalid() {
-	~/dev/ccompiler/build/zcc --delete-asm $1
-	if [ $? == 1 ]
-	then
-		echo -e "${GREEN}[Passed] $1 ${CLEAR}"
-	else
-		echo -e "${RED}[FAILED] $1 ${CLEAR}"
-	fi
+       ~/dev/ccompiler/build/zcc --delete-asm $1
+       if [ $? == 1 ]
+       then
+               echo -e "${GREEN}[Passed] $1 ${CLEAR}"
+       else
+               echo -e "${RED}[FAILED] $1 ${CLEAR}"
+       fi
 }
-
 
 echo "[Info] testing valid code"
 test_program not_zero.c 1
